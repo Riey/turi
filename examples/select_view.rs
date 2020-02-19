3d8e69d9ac277f8c80e6182cbc62a2918cbf9e42
Riey <creeper844@gmail.com>
2020-02-19 21:27:44 +0900

    Update select_view example

--- examples/select_view.rs
+++ examples/select_view.rs
@@ -1,29 +1,36 @@
-use crossterm::event::{
-    Event,
-    KeyCode,
-    KeyEvent,
-    KeyModifiers,
+use crossterm::{
+    cursor::{Show, Hide},
+    event::{
+        DisableMouseCapture,
+        EnableMouseCapture,
+        Event,
+        KeyCode,
+        KeyEvent,
+        MouseButton,
+        MouseEvent,
+    },
+    terminal::{
+        disable_raw_mode,
+        enable_raw_mode,
+        EnterAlternateScreen,
+        LeaveAlternateScreen,
+    },
 };
 use simplelog::*;
-use std::io::BufWriter;
-use turi::{
-    printer::PrinterGuard,
-    view::View,
-    views::SelectView,
+use std::io::{
+    BufWriter,
+    Write,
 };
-
-fn quit_check<S>(
-    _s: &mut S,
-    e: Event,
-) -> Option<bool> {
-    match e {
-        Event::Key(KeyEvent {
-            code: KeyCode::Char('c'),
-            modifiers: KeyModifiers::CONTROL,
-        }) => Some(true),
-        _ => None,
-    }
-}
+use turi::{
+    backend::{Backend, CrosstermBackend},
+    printer::Printer,
+    view::View,
+    views::{
+        SelectView,
+        SelectViewEvent,
+        SelectViewMessage,
+    },
+};
 
 fn main() {
     WriteLogger::init(
@@ -37,14 +44,76 @@ fn main() {
     let out = std::io::stdout();
     let out = out.lock();
     let mut out = BufWriter::with_capacity(1024 * 1024, out);
-    let mut printer_guard = PrinterGuard::new(&mut out, true);
 
-    let mut select_view = SelectView::with_items(vec![("123".into(), 123), ("456".into(), 456)])
-        .map(|view, _, _| {
-            log::trace!("Input: {}", view.selected_val());
-            false
+    enable_raw_mode().unwrap();
+    crossterm::execute!(out, Hide, EnterAlternateScreen, EnableMouseCapture).unwrap();
+
+    let mut backend = CrosstermBackend::new(&mut out, crossterm::terminal::size().unwrap().into());
+
+    let mut state = ();
+
+    let mut view = SelectView::with_items(vec![("123".into(), 123), ("456".into(), 456)])
+        .map(|view, _state, msg| {
+            match msg {
+                SelectViewMessage::Select => {
+                    log::info!("Selected: {}", view.selected_val());
+                    true
+                }
+                msg => {
+                    log::info!("Other event: {:?}", msg);
+                    false
+                }
+            }
         })
-        .map_e(quit_check);
+        .map_opt_e(|_view, _state, event| {
+            match event {
+                Event::Key(KeyEvent { code, .. }) => {
+                    match code {
+                        KeyCode::Enter => Some(SelectViewEvent::Enter),
+                        KeyCode::Up => Some(SelectViewEvent::Up),
+                        KeyCode::Down => Some(SelectViewEvent::Down),
+                        _ => None,
+                    }
+                }
+                Event::Mouse(MouseEvent::Down(MouseButton::Left, _, y, ..)) => {
+                    Some(SelectViewEvent::Click(y))
+                }
+                _ => None,
+            }
+        });
 
-    turi::run(&mut (), &mut select_view, &mut printer_guard);
+    view.render(&mut Printer::new(&mut backend));
+    backend.flush();
+
+    loop {
+        let e = crossterm::event::read().unwrap();
+
+        match e {
+            Event::Key(KeyEvent {
+                code: KeyCode::Char('q'),
+                ..
+            }) => {
+                break;
+            }
+            Event::Resize(x, y) => {
+                backend.resize((x, y).into());
+            }
+            e => {
+                match view.on_event(&mut state, e) {
+                    Some(msg) => {
+                        view.render(&mut Printer::new(&mut backend));
+                        backend.flush();
+                        if msg {
+                            break;
+                        }
+                    }
+                    None => {}
+                }
+            }
+        }
+    }
+
+    crossterm::execute!(out, DisableMouseCapture, LeaveAlternateScreen, Show).unwrap();
+
+    disable_raw_mode().unwrap();
 }
