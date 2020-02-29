3816fdfd0546eba3c28ab472652775312c8954c5
Riey <creeper844@gmail.com>
2020-02-29 18:59:54 +0900

    Make find_str_width_pos

--- src/util.rs
+++ src/util.rs
@@ -1,19 +1,25 @@
 use unicode_width::UnicodeWidthChar;
 
 #[inline]
-pub fn slice_str_with_width(text: &str, mut width: usize) -> (&str, &str, usize) {
+pub fn slice_str_with_width(text: &str, width: usize) -> (&str, &str, usize) {
+    let (i, width) = find_str_width_pos(text, width);
+    unsafe {
+        (text.get_unchecked(..i), text.get_unchecked(i..), width)
+    }
+}
+
+#[inline]
+pub fn find_str_width_pos(text: &str, mut width: usize) -> (usize, usize) {
     for (i, ch) in text.char_indices() {
         match width.checked_sub(ch.width().unwrap_or(0)) {
             Some(new_width) => {
                 width = new_width;
             }
             None => {
-                return unsafe {
-                    (text.get_unchecked(..i), text.get_unchecked(i..), width)
-                };
+                return (i, width);
             }
         }
     }
 
-    (text, "", 0)
+    (text.len(), width)
 }
